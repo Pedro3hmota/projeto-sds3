47bc673fd55db7e156c6fe9c83f268a3158a42b8
Pedro Henrique <pedrohenriques.mota33@outlook.com>
2021-09-11 23:32:57 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
-import { BASE_URL } from 'utils/request';
+import { BASE_URL } from 'utils/requests';
+
 
 type ChartData = {
     labels: string[];
@@ -23,12 +24,6 @@ function DonutChart() {
                 setChartData({ labels: myLabels, series: mySeries });
             });
     }, []);
-
-
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
 
     const options = {
         legend: {
